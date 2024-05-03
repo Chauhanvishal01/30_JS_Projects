53ca952bb26d2d6dd810dff58f4544f5e689cd10
Vishal Chauhan <151999539+Chauhanvishal01@users.noreply.github.com>
2024-05-03 18:00:13 +0530

    Hide_And_Show_Password

---
+++ 14_Hide_&_Display/app.js
@@ -0,0 +1,14 @@
+const password = document.getElementById("password");
+const icon = document.getElementById("icon");
+
+icon.addEventListener("click", () => {
+  if (password.type == "password") {
+    password.type = "text";
+    icon.src = "eye-open.png";
+  } else {
+    password.type = "password";
+    icon.src = "eye-close.png";
+
+
+  }
+});
